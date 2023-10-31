a45dcf18195bdae8b3bd50c342d0dd3de5b27320
bujordragos <149311370+bujordragos@users.noreply.github.com>
2023-10-31 21:53:34 +0200

    Baza de date intreaga

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,10 @@
 const mysql = require('mysql2');
 const utils = require("./utils");
 const { inscrisisDb } = require('./utils');
+const express = require('express');
+const app = express();
+const port = 3000;  // Replace with your desired port number
+
 const connection = mysql.createConnection({
   host: 'localhost',  // Replace with your database host
   user: 'root',       // Replace with your database username
@@ -20,9 +24,6 @@ connection.connect((err) => {
 // Close the connection when you're done with it
 connection.end();
 
-const express = require('express');
-const app = express();
-const port = 3000;  // Replace with your desired port number
 
 // Import the cors middleware
 const cors = require('cors');
@@ -42,12 +43,51 @@ app.get('/reset', async (req, res) => {
     res.status(500).send("Eroare la resetarea bazei de date: " + error);
   }
 });
+function resetAutoIncrement() {
+  const resetQuery = 'ALTER TABLE inscrisiDb AUTO_INCREMENT = 1'; // Replace 'your_table_name' with your actual table name
+
+  connection.query(resetQuery, (err, results) => {
+    if (err) {
+      console.error('Error resetting auto-increment:', err);
+      return;
+    }
+    console.log('Auto-increment reset successfully.');
+  });
+}
 
 app.post('/register', async (req, res) => {
   console.log("Request Data:", req.body); // Log the request data
 
   const { prenume, nume, email, phone, varsta } = req.body;
 
+  // Validation checks
+  const prenumeValid = /^[A-Za-z]+$/.test(prenume);
+  const numeValid = /^[A-Za-z]+$/.test(nume);
+  const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  const phoneValid = /^\d{10}$/.test(phone);
+  const varstaValid = /^\d{1,2}$/.test(varsta) && parseInt(varsta) >= 0 && parseInt(varsta) <= 99;
+
+  if (!prenumeValid) {
+    return res.status(400).send("Prenume must contain only letters.");
+  }
+
+  if (!numeValid) {
+    return res.status(400).send("Nume must contain only letters.");
+  }
+
+  if (!emailValid) {
+    return res.status(400).send("Email must be in a valid format.");
+  }
+
+  if (!phoneValid) {
+    return res.status(400).send("Phone must be a 10-digit number.");
+  }
+
+  if (!varstaValid) {
+    return res.status(400).send("Varsta must be a 2-digit number between 0 and 99.");
+  }
+
+  // If all validations pass, proceed to register the user
   try {
     const result = await inscrisisDb.create({
       prenume,
@@ -66,3 +106,4 @@ app.post('/register', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
--- back-end/utils.js
+++ back-end/utils.js
@@ -14,6 +14,11 @@ const database = new Sequelize("formular_inregistrare", "root", "", {
 
 // Define the "inscrisis" model
 const inscrisisDb = database.define("inscrisis", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true, // This sets the ID to auto-increment
+  },
   prenume: {
     type: DataTypes.STRING,
     allowNull: false, // Ensure allowNull is set to false for all required fields
